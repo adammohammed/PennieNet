c98903344249a6c8f4275013d9783aaf40ebd911
Adam <Adam@SLEEPEROP>
2016-06-14 17:00:51 -0400

    Implementing Bluetooth-notfinished

---
+++ PennieNet/Commander.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PennieNet
+{
+    internal class Commander
+    {
+        public Commander()
+        {
+            // Connect to bluetooth
+        } 
+        public void IssueCmd(string input)
+        {
+            if (input.Equals("left"))
+            {
+
+            }
+            else if (input.Equals("right"))
+            {
+
+            }
+            else if (input.Equals("fwd"))
+            {
+
+            }
+            else if (input.Equals("rev"))
+            {
+
+            }
+            else
+            {
+                //STOP!!
+            }
+        }
+    }
+}
