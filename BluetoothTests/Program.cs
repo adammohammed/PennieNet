68c7df0075ae96b6dd0c3ae8502bbd3276fd9b69
Adam <Adam@SLEEPEROP>
2016-06-20 15:28:47 -0400

    Test Bluetooth

---
+++ BluetoothTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PennieNet;
+
+namespace BluetoothTests
+{
+    class Program
+    {
+        static Commander cmd;
+        static void Main(string[] args)
+        {
+            cmd = new Commander();
+            cmd.Connect();
+            for (;;)
+            {
+                if (cmd != null)
+                {
+                    var data = Console.ReadLine();
+                    cmd.IssueCmd(data);
+                }
+            }
+        }
+    }
+}
