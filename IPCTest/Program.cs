4e2fd21d80c86aee3be01b7ee5b7675f826e0741
Adam <Adam@SLEEPEROP>
2016-07-05 14:37:04 -0400

    Added IPC testing project

---
+++ IPCTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace IPCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+
+            client.Connect("localhost", 3050);
+            while (client.Connected)
+            {
+                Console.Write("Enter a test val: ");
+                var str = Console.ReadLine();
+
+                using (StreamWriter sw = new StreamWriter(client.GetStream()))
+                {
+                    sw.WriteLine(str);
+                    using (StreamReader sr = new StreamReader(client.GetStream()))
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+
+            }
+        }
+    }
+}
